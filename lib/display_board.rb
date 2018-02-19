af33762d15546ac24911e826d248c128cebd0975
Jaclyn Hall <jaclynchall@gmail.com>
2018-02-19 20:46:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  cell = "   |   |   "
-  pipe = "-----------"
-  puts cell
-  puts pipe
-  puts cell
-  puts pipe
-  puts cell
+  puts " #{board[0]}  | #{board[1]}  |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
